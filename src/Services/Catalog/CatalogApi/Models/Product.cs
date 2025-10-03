b13f164ae9999f083da19af9300494afc5e28db6
Cleyton da Silva Neves <CleytondaSilva.Neves-ext@ab-Inbev.com>
2025-10-03 17:22:49 -0300

    Progresso do desenvolvimento.

---
+++ src/Services/Catalog/CatalogApi/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace CatalogApi.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<string> Category { get; set; } = new();
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
